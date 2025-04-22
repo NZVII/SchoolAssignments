68e2f1cf4cb0bab609816bcb713eb61923f23bac
nz <aureulan@gmail.com>
2025-04-21 19:50:59 -0700

    development

--- d03.cpp
+++ d03.cpp
@@ -4,32 +4,25 @@
 # author:           Alan Diaz
 # lab:              Discussion #3
 # date:             April 12, 2025
-# description:      This program prompts for a vehicle's mpg and the price per
-                    gallon then calculates the total cost per gallon for
-                    20, 75, and 500 miles.
+# description:      This program prompts the user for a total change amount
+                    and then outputs the change using the fewest coins
 
 # input:            double carmpg, double costpergallon
 # output:           double totalgascost
 # sources:          none
 #******************************************************************************/
-
-#include <iomanip>
 #include <iostream>
 using namespace std;
 int main (){
     // declare variables
     int promptTotalChange;
-
     int changeDollars;
-
     int changeQuarters;
     int changeDimes;
     int changeNickels;
     int changePennies;
-
+    //tracks the running total of cents
     int totalCents;
-
-
     // inputs
     cin >> promptTotalChange;
 
@@ -44,13 +37,13 @@ int main (){
         totalCents = promptTotalChange;
     }
 
-    // if there is no remaining change, set all to 0
-
     // process remaining change
     // evaluate quarters
     if (totalCents % 25 == 0){
         changeQuarters = totalCents / 25;
-    } else{
+        totalCents = 0;
+    }
+    else{
         int remainder = totalCents % 25;
         changeQuarters = (totalCents - remainder) / 25;
         totalCents = remainder;
@@ -59,40 +52,55 @@ int main (){
     // evaluate dimes
     if (totalCents % 10 == 0){
         changeDimes = totalCents / 10;
+        totalCents = 0;
+
     } else {
         int remainder = totalCents % 10;
         changeDimes = (totalCents - remainder) / 10;
         totalCents = remainder;
     }
-
-
+    //evaluate nickels
+    if (totalCents % 5 == 0){
+        changeNickels = totalCents / 5;
+        totalCents = 0;
+    } else {
+        int remainder = totalCents % 5;
+        changeNickels = (totalCents - remainder) / 5;
+        totalCents = remainder;
+    }
+    // evaluate pennies
+    if (totalCents % 1 == 0){
+        changePennies = totalCents;
+    }
     // outputs
     // dollar output
-    cout << changeDollars << " ";
-    if (changeDollars == 1){
+    if (changeDollars == 1 && changeDollars != 0){
+        cout << changeDollars << " ";
         cout << "Dollar" << endl;
     }
     else {
+        cout << changeDollars << " ";
         cout << "Dollars" << endl;
     }
     //quarter output
-    cout << changeQuarters << " ";
-    if (changeQuarters == 1){
+    if (changeQuarters == 1 && changeQuarters != 0){
+        cout << changeQuarters << " ";
         cout << "Quarter" << endl;
     }
-    else {
-        cout << "Quarters" << endl;
-    }
     //dime output
-    cout << changeDimes << " ";
-    if (changeQuarters == 1){
+    if (changeDimes == 1 && changeDimes !=0){
+        cout << changeDimes << " ";
         cout << "Dime" << endl;
     }
-    else {
-        cout << "Dimes" << endl;
+    //nickel output
+    if (changeNickels == 1 && changeNickels != 0 ){
+        cout << changeNickels << " ";
+        cout << "Nickel" << endl;
     }
-
-
-
+    //pennies output
+    if (changePennies == 1 && changePennies !=0){
+        cout << changePennies << " ";
+        cout << "Penny" << endl;
+    }
     return 0;
 }
