0320a68a27fd9af20b765a217166099a491e317c
nz <aureulan@gmail.com>
2025-04-24 14:25:26 -0700

    a04

--- a04.cpp
+++ a04.cpp
@@ -12,6 +12,7 @@
 // Neither comments nor code should be wider than 79 characters.
 // The lines of asterisks above are 79 characters long for easy reference.
 #include <iostream>
+#include <variant>
 using namespace std;
 
 int main(){
@@ -24,13 +25,30 @@ int main(){
 
     // user inputs
     cout << "Enter phrase one: ";
-    cin >> phraseOne;
+    getline(cin, phraseOne);
 
     cout << "Enter phrase two: ";
-    cin >> phraseTwo;
+    getline(cin, phraseTwo);
 
+
+    // store the lengths of the phrases
     const int PHRASE_ONE_LENGTH = phraseOne.length();
     const int PHRASE_TWO_LENGTH = phraseTwo.length();
+
+    // evaluate phrases
+    // find phraseOne in phraseTwo
+
+    size_t found = phraseTwo.find(phraseOne);
+    size_t foundTest = phraseTwo.find("hello world");
+    // issue wher
+    cout << phraseOne << " " << foundTest << endl;
+    if (found != string::npos){
+        cout << "\"" << phraseOne << "\""
+             << " was found in "
+             << "\"" << phraseTwo << "\""
+             << endl;
+    }
+
 
 
     return 0;
