46d1bf53931d84ac540019f91cdd153dd465ec5d
nz <aureulan@gmail.com>
2025-05-08 14:20:21 -0700

    a05 starting

---
+++ a05.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+# Author:           Alan Diaz
+# Assignment:       A5 (CS161A)
+# Date:             May 8, 2025
+# Description:
+# Input:
+# Output:
+# Sources:
+#******************************************************************************/
+// Neither comments nor code should be wider than 79 characters.
+// The lines of asterisks above are 79 characters long for easy reference.
+// Neither comments nor code should be wider than 79 characters.
+// The lines of asterisks above are 79 characters long for easy reference.
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  double tax = 0.07;
+  double deliveryFee = 0.2;
+
+  // party inputs
+  int numAttendees, avgSlicesPerAttendees;
+  double singlePizzaCost;
+
+  int numPizzas;
+  double totalPartyCost, partyPizzasCost;
+  int totalNumPizzas, totalNumParties;
+  int maxNumPeople = 0;
+  double maxPartyCost = 0;
+
+  cout << "Welcome to my Pizza Party Statistics program!" << endl;
+  char option;
+  do{
+
+  }
+  while(option != 'n');
+
+  return 0;
+}
