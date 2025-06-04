54d644aebe5ae785af5f010445388a3438577db5
nz <aureulan@gmail.com>
2025-06-04 12:43:11 -0700

    d09 begin

---
+++ d09.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+# Author:           Alan Diaz
+# Lab:              Discussion #9
+# Date:             June 4, 2025
+# Description:
+#
+# Input:
+# Output:
+# Sources:          None
+#******************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+
+// Function Prototypes
+int reverseDigit(int num);
+
+int main()
+{
+   int numReverse;
+   int input;
+   // input prompt
+   cout << "Enter an integer: ";
+   cin >> input;
+
+   numReverse = reverseDigit(input);
+
+   // output
+   cout << "\n\nThe value of reverseDigit(" << input << ") is: "
+        << numReverse;
+
+   return 0;
+}
+
+int reverseDigit(int num){
+    string numString = to_string(num);
+    vector<char> numVectorReverse(numString.size());
+    for (unsigned int i = 0; i < numString.size(); ++i){
+        numVectorReverse.push_back(numString.at((numString.length()-1)-i));
+    }
+    string reverseNumString;
+    reverseNumString = numVectorReverse;
+}
