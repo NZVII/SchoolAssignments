33afc7e0acfb217859fb518c19744cd15938fbd0
nz <auruelan@gmail.com>
2025-11-21 18:32:59 -0800

    development

---
+++ CS-161B/d07.cpp
@@ -0,0 +1,39 @@
+/******************************************************************************
+# Author:           Alan Diaz
+# Assignment:       d07  (CS161B)
+# Date:             November 21, 2025
+# Description:
+# Input: Program asks for the user inputs
+# Output: Program outputs the discount, totalCost, finalCost, and tip
+# Sources:
+#******************************************************************************/
+// Neither comments nor code should be wider than 79 characters.
+// The lines of asterisks above are 79 characters long for easy reference.
+// Neither comments nor code should be wider than 79 characters.
+// The lines of asterisks above are 79 characters long for easy reference.
+#include <iostream>
+#include <iomanip>
+#include <cctype>
+using namespace std;
+
+// declare function prototypes
+void welcome();
+
+int main(){
+    // declare variables
+    return 0;
+}
+
+
+//Name:   welcome()
+//Desc:   This function displays a welcome message to the user
+//input:  none
+//output: welcome message
+//return: void
+void welcome(){
+    cout << "Welcome to my Discussion 6 Program!" << endl
+        << "This program stores user information about vehicles"
+        << "they own or like by prompting for the # of vehicles "
+        << "they would like to add and prompting for its year,make,model."
+        << endl;
+}
